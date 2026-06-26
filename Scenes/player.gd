7abec0386a30b9985419e82fb96fa554e20eedaf
Maruf Khan <maruf1611khan@gmail.com>
2026-06-27 01:08:16 +0530

    feat: initialize project structure and import game assets, added basic player movement

---
+++ Scenes/player.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@export var speed : int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(400, 500)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * speed * delta
